7a39201b51ccb120b5e3485f6eb8c9ff36b5d965
Jorge Celaya <jcelaya775@gmail.com>
2023-10-02 20:18:40 -0500

    Fix c-del

--- after/plugin/vim-illuminate.lua
+++ after/plugin/vim-illuminate.lua
@@ -1,8 +1,8 @@
 local illuminate = require("illuminate")
 
-vim.keymap.set({ "n", "v", "x" }, "<A-n>", function()
+vim.keymap.set({ "v", "x" }, "<A-n>", function()
 	illuminate.goto_next_reference()
 end)
-vim.keymap.set({ "n", "v", "x" }, "<A-p>", function()
+vim.keymap.set({ "v", "x" }, "<A-p>", function()
 	illuminate.goto_prev_reference()
 end)
