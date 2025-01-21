d838e60852027c8cc1d8dc73770faa4deb56654a
Jorge Celaya <jcelaya775@gmail.com>
2025-01-21 17:46:58 -0600

    remove leap.nvim

--- after/plugin/leap.lua
+++
@@ -1,4 +0,0 @@
--- vim.keymap.set({ "n", "v", "x" }, "<A-/>", "<Plug>(leap-forward-to)")
--- vim.keymap.set({ "n", "v", "x" }, "<M-?>", "<Plug>(leap-backward-to)")
--- vim.keymap.set({ "n", "v", "x" }, "<A-[>", "<Plug>(leap-forward-to)")
--- vim.keymap.set({ "n", "v", "x" }, "<A-{>", "<Plug>(leap-backward-to)")
