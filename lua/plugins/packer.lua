96c5dcf2c1a903af9a58c5e8f0d8b56255c61840
Jorge Celaya <jcelaya775@gmail.com>
2025-06-01 21:16:31 -0500

    neovim(plugins): update to harpoon2

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -27,7 +27,11 @@ return require("packer").startup(function(use)
   use("tpope/vim-rhubarb")
   use("nvim-lua/plenary.nvim")
   use("simeji/winresizer")
-  use("ThePrimeagen/harpoon")
+  use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { { "nvim-lua/plenary.nvim" } }
+  }
   -- use 'romgrk/barbar.nvim'
   use("terrortylor/nvim-comment")
   use({
